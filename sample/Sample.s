8f29454f1eb40963ac6a3efe62410201823fceee
Jack Shell <jackshel45@gmail.com>
2021-01-14 18:25:14 +0200

    Fixed to pass tests

--- sample/Sample.s
+++ sample/Sample.s
@@ -7,11 +7,11 @@ let structure =
 {
   a : 1,
   b : 's',
-  c : [ 1,3 ],
-  d : [ 1,{ date : new Date() } ],
+  c : [ 1, 3 ],
+  d : [ 1, { date : new Date() } ],
   e : function(){},
   f : new ArrayBuffer( 13 ),
-  g : new Float32Array([ 1,2,3 ]),
+  g : new Float32Array([ 1, 2, 3 ]),
 }
 
 function handleUp( e, k, it )
